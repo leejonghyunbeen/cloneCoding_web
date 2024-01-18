6b7d13c8f76245b461f28f634320fad9ec2ee2cd
leejonghyunbeen <clfl19@naver.com>
2024-01-18 15:30:47 +0900

    쇼핑몰 연습

---
+++ shopping_database/script/data.js
@@ -0,0 +1,63 @@
+//상품 dd 저장위치
+const itemDB = [
+    {
+        itemImg:'./images/iceCream.jpg',
+        itemTitle:'식빵 플레이트 접시 세트',
+        itemPrice:'14,900원',
+    },
+    {
+        itemImg:'./images/iceCream.jpg',
+        itemTitle:'핸드크림 미니 트리오',
+        itemPrice:'39,000원',
+    },
+    {
+        itemImg:'./images/iceCream.jpg',
+        itemTitle:'달래 200G',
+        itemPrice:'5,990원',
+    },
+    {
+        itemImg:'./images/iceCream.jpg',
+        itemTitle:'청경채 500G',
+        itemPrice:'3,990원',
+    },
+    {
+        itemImg:'./images/iceCream.jpg',
+        itemTitle:'만차량 단호박 1통',
+        itemPrice:'5,490원',
+    },
+    {
+        itemImg:'./images/iceCream.jpg',
+        itemTitle:'덴마크 유산균이야기',
+        itemPrice:'59,900원',
+    },
+    {
+        itemImg:'./images/iceCream.jpg',
+        itemTitle:'로쉐 컬렉션 15개입',
+        itemPrice:'15,480원',
+    },
+    {
+        itemImg:'./images/iceCream.jpg',
+        itemTitle:'로쉐 사각 8개입',
+        itemPrice:'7,480원',
+    },
+    {
+        itemImg:'./images/iceCream.jpg',
+        itemTitle:'아연 이뮨케이(30일분)',
+        itemPrice:'5,9000원',
+    },
+    {
+        itemImg:'./images/iceCream.jpg',
+        itemTitle:'블랙 래빗 키링(30인분)',
+        itemPrice:'9,900원',
+    },
+    {
+        itemImg:'./images/iceCream.jpg',
+        itemTitle:'피오나 삼푸400ml',
+        itemPrice:'35,900원',
+    },
+    {
+        itemImg:'./images/iceCream.jpg',
+        itemTitle:'피오나 삼슈우400ml',
+        itemPrice:'35,900   0원',
+    },
+]
